a3ebd048b41f4e8f4e4b2ce3ca535bd027e995b7
xanderkiev2022 <301019a@gmail.com>
2023-05-07 21:58:51 +0300

    selector added

--- redux/auth/authSelectors.js
+++ redux/auth/authSelectors.js
@@ -5,3 +5,4 @@ export const selectPhoto = state => state.auth.photo;
 export const selectIsLoggedIn = state => state.auth.isLoggedIn;
 export const selectIsLoading = state => state.auth.isLoading;
 export const selectError = state => state.auth.error;
+export const selectRefreshing = state => state.auth.isRefreshing;
