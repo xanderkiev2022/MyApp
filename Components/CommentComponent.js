f6bebf9da343c3ed7f603b63a56f15cb88e9eeea
xanderkiev2022 <301019a@gmail.com>
2023-05-25 20:52:55 +0300

    del comment, pop up menu

--- Components/CommentComponent.js
+++ Components/CommentComponent.js
@@ -1,48 +1,69 @@
-import React from 'react';
-import { StyleSheet, View, Text, Image } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, Image, Modal, TouchableWithoutFeedback } from 'react-native';
 import { useSelector } from 'react-redux';
 import { selectUserId } from '../redux/auth/authSelectors';
 import moment from 'moment-timezone';
 import { TouchableOpacity } from 'react-native';
 moment.tz.setDefault('Europe/Kiev');
+import { FontAwesome } from '@expo/vector-icons';
 
 export default function CommentComponent({ item, onDeleteComment }) {
   const { comment, date, userId, photo } = item;
   const myId = useSelector(selectUserId);
+  const [modalVisible, setModalVisible] = useState(false);
 
   const handleDelete = () => {
     onDeleteComment(item.id);
-  };
+    setModalVisible(false);
+     };
 
-  const renderDeleteButton = () => {
+  const handlePress = () => {
     if (userId === myId) {
-      return (
-        <TouchableOpacity onPress={handleDelete}>
-          <Text style={styles.deleteButton}>Delete</Text>
-        </TouchableOpacity>
-      );
+      setModalVisible(true);
     }
     return null;
   };
 
+    const handleModalClose = () => {
+      setModalVisible(false);
+    };
+
   return (
     <View style={{ ...styles.container, flexDirection: userId !== myId ? 'row' : 'row-reverse' }}>
       <Image source={{ uri: photo }} style={{ ...styles.avatar, marginLeft: userId !== myId ? 0 : 16, marginRight: userId !== myId ? 16 : 0 }} />
-      <View
-        style={{
-          ...styles.commentContainer,
-          marginLeft: userId !== myId ? 16 : 0,
-          borderTopStartRadius: userId !== myId ? 0 : 6,
-          borderTopEndRadius: userId !== myId ? 6 : 0,
-        }}
-      >
-        <Text style={styles.commentText}>{comment}</Text>
-        <Text style={styles.commentDate}>
-          {moment(date.seconds * 1000).format('DD MMMM, YYYY')}&nbsp;|&nbsp;
-          {moment(date.seconds * 1000).format('hh:mm')}
-        </Text>
-      </View>
-      {renderDeleteButton()}
+
+      <TouchableWithoutFeedback onPress={handlePress}>
+        <View
+          style={{
+            ...styles.commentContainer,
+            marginLeft: userId !== myId ? 16 : 0,
+            borderTopStartRadius: userId !== myId ? 0 : 6,
+            borderTopEndRadius: userId !== myId ? 6 : 0,
+            backgroundColor: modalVisible ? 'rgba(0, 0, 0, 0.25)' : 'rgba(0, 0, 0, 0.03)',
+          }}
+        >
+          <Text style={styles.commentText}>{comment}</Text>
+          <Text style={styles.commentDate}>
+            {moment(date.seconds * 1000).format('DD MMMM, YYYY')}&nbsp;|&nbsp;
+            {moment(date.seconds * 1000).format('hh:mm')}
+          </Text>
+        </View>
+      </TouchableWithoutFeedback>
+
+      <Modal animationType="fade" transparent visible={modalVisible}>
+        <TouchableWithoutFeedback onPress={handleModalClose}>
+          <View style={styles.modalContainer}>
+            <TouchableWithoutFeedback>
+              <View style={styles.modalContent}>
+                <TouchableOpacity style={styles.deleteButtonContainer} onPress={handleDelete}>
+                  <FontAwesome name="trash" size={20} color="#BDBDBD" />
+                  <Text style={styles.deleteButton}>Delete</Text>
+                </TouchableOpacity>
+              </View>
+            </TouchableWithoutFeedback>
+          </View>
+        </TouchableWithoutFeedback>
+      </Modal>
     </View>
   );
 };
@@ -79,5 +100,31 @@ const styles = StyleSheet.create({
     lineHeight: 12,
     color: '#BDBDBD',
     textAlign: 'right',
+  },
+
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContent: {
+    backgroundColor: 'white',
+    padding: 16,
+    borderRadius: 6,
+    width: 150,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+
+  deleteButtonContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  deleteButton: {
+    fontFamily: 'RobotoRegular',
+    fontSize: 16,
+    color: '#212121',
+    marginLeft: 8,
   },
 });
