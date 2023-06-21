8b369e0b71e2cbffc9170db4f812b1cbacefbab6
xanderkiev2022 <301019a@gmail.com>
2023-06-21 14:07:00 +0300

    checkBox added

--- Screens/SearchScreen.js
+++ Screens/SearchScreen.js
@@ -2,21 +2,48 @@ import React, { useState } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 import { BgImage } from '../Components/BgImage';
 import Slider from '../Components/Slider';
+import { CheckBox } from '../Components/CheckBox';
 
 export default function SearchScreen({ navigation }) {
+
+  // slider
   const [sliderAge, setSliderAge] = useState(20); // Початкове значення sliderAge
+
+  // checkBox
+  const [green, setGreen] = useState(false);
+  const [blue, setBlue] = useState(false);
+
+  const fields = [
+    { text: 'Blue color', disabled: false, isSelected: blue, setIsSelected: setBlue },
+    { text: 'Green color', disabled: false, isSelected: green, setIsSelected: setGreen },
+  ];
 
   return (
     <View style={styles.container}>
       <BgImage>
         <View style={styles.regScr}>
-          <Text>Set your preferences</Text>
+          <Text style={{ ...styles.titleText, textAlign: 'center' }}>Set your preferences</Text>
           <View style={styles.sliderContainer}>
             <View style={styles.sliderHeader}>
               <Text>Age</Text>
               <Text style={styles.sliderAge}>{sliderAge}-45</Text>
             </View>
             <Slider sliderAge={sliderAge} setSliderAge={setSliderAge} />
+          </View>
+          <View style={styles.checkboxTitle}>
+            <Text style={styles.titleText}>Title of CheckBox</Text>
+            {fields.map(field => (
+              <View style={styles.checkboxContainer}>
+                <CheckBox
+                  disabled={field.disabled}
+                  isSelected={field.isSelected}
+                  onSelectChange={() => {
+                    field.setIsSelected(!field.isSelected);
+                  }}
+                />
+                <Text style={styles.checkboxText}>{field.text}</Text>
+              </View>
+            ))}
           </View>
         </View>
       </BgImage>
@@ -29,16 +56,29 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   regScr: {
-    // width: '100%',
+    paddingTop: 16,
     paddingHorizontal: 16,
     height: '85%',
     backgroundColor: '#FFFFFF',
     borderTopLeftRadius: 25,
     borderTopRightRadius: 25,
   },
-  sliderContainer: {},
+  sliderContainer: {
+    marginBottom: 20,
+  },
   sliderHeader: {
     flexDirection: 'row',
-    justifyContent: 'space-between',
+    justifyContent: 'space-between',    
+  },
+  checkboxTitle: {
+  },
+  titleText: {
+    fontWeight: 'bold',
+    fontSize: 19,
+    marginBottom: 10,
+  },
+  checkboxContainer: {
+    flexDirection: 'row',
+    marginBottom: 5,
   },
 });
